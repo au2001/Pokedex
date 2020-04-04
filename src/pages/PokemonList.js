af2de031023c85ce504e66dae3beb015123231cc
Aurélien <au2001sfg@gmail.com>
2020-04-04 18:15:23 +0200

    Create Pokémon list cards

--- src/pages/PokemonList.js
+++ src/pages/PokemonList.js
@@ -2,8 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Pokedex from '../Pokedex';
 
+import './PokemonList.css';
+
+class PokemonCard extends React.Component {
+    render() {
+        return (
+            <Link to={`/pokemon/${Pokedex.slug(this.props.pokemon)}`} class="pokemon-card">
+                <img src={this.props.pokemon.img} alt={this.props.pokemon.name} title={this.props.pokemon.name} class="pokemon-image" />
+                <span class="pokemon-number">#{this.props.pokemon.num}</span>
+                <span class="pokemon-name">{this.props.pokemon.name}</span>
+                <span class="pokemon-types">
+                    {this.props.pokemon.type.map(type =>
+                        <span class={`pokemon-type type-${type.toLowerCase()}`}>{type}</span>
+                    )}
+                </span>
+            </Link>
+        );
+    }
+}
+
 export default class PokemonList extends React.Component {
-	constructor() {
+    constructor() {
         super();
 
         this.state = {
@@ -13,17 +32,13 @@ export default class PokemonList extends React.Component {
 
     render() {
         return (
-        	<div>
-            	<h1>Liste de Pokémons</h1>
-            	<ul>
-            		{this.state.pokemons.map(pokemon => {
-            			return (
-            				<li key={pokemon.id}>
-            					<Link to={`/pokemon/${Pokedex.slug(pokemon)}`}>{pokemon.name}</Link>
-            				</li>
-            			);
-            		})}
-            	</ul>
+            <div>
+                <h1>Liste de Pokémons</h1>
+                <div class="pokemon-list">
+                    {this.state.pokemons.map(pokemon =>
+                        <PokemonCard pokemon={pokemon} />
+                    )}
+                </div>
             </div>
         );
     }
