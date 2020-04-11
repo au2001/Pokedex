4d5e6adf21add199660a54d56a8b48befa8b834d
Aurélien <au2001sfg@gmail.com>
2020-04-11 13:50:56 +0200

    Create Pokémon details page

--- src/pages/PokemonDetails.js
+++ src/pages/PokemonDetails.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Pokedex from '../Pokedex';
 import NotFound from './NotFound';
+import PokemonCard from '../components/PokemonCard';
 
 import './PokemonDetails.css';
 
@@ -10,8 +11,25 @@ export default class PokemonDetails extends React.Component {
         super(props);
 
         this.state = {
-            pokemon: Pokedex.search(props.match.params.pokemonName)
-        };
+            pokemon: null,
+            prev_evolution: undefined,
+            next_evolution: undefined
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        const pokemon = Pokedex.search(props.match.params.pokemonName);
+        if (pokemon !== state.pokemon) {
+            return {
+                pokemon: pokemon,
+                prev_evolution: pokemon.prev_evolution !== undefined ? pokemon.prev_evolution.map(evolution =>
+                    Pokedex.search(evolution.num + " " + evolution.name)
+                ) : undefined,
+                next_evolution: pokemon.next_evolution !== undefined ? pokemon.next_evolution.map(evolution =>
+                    Pokedex.search(evolution.num + " " + evolution.name)
+                ) : undefined
+            };
+        }
     }
 
     render() {
@@ -28,8 +46,85 @@ export default class PokemonDetails extends React.Component {
                         <img src="/back.png" alt="Retour" />
                     </Link>
                     <img src="/logo.png" alt="Logo Pokédex" className="page-title-logo" />
-                    <span className="page-title-text">{this.state.pokemon.name}</span>
+                    <span className="page-title-text">{this.state.pokemon.name} #{this.state.pokemon.num}</span>
                 </h1>
+
+                <div className="pokemon-details">
+                    <div className="pokemon-details-photo-container">
+                        <img src={this.state.pokemon.img} alt={this.state.pokemon.name} className="pokemon-details-photo" />
+                    </div>
+                    <div className="pokemon-details-info">
+                        <span className="pokemon-details-label">Types&nbsp;:</span>
+                        <span className="pokemon-details-value">
+                            {this.state.pokemon.type.map(type =>
+                                <span key={type.toLowerCase()} className={`pokemon-type type-${type.toLowerCase()}`}>{type}</span>
+                            )}
+                        </span><br/>
+
+                        <span className="pokemon-details-label">Hauteur&nbsp;:</span>
+                        <span className="pokemon-details-value">{this.state.pokemon.height}</span><br/>
+
+                        <span className="pokemon-details-label">Poids&nbsp;:</span>
+                        <span className="pokemon-details-value">{this.state.pokemon.weight}</span><br/>
+
+                        {this.state.pokemon.candy !== "None" && (
+                            <span>
+                                <span className="pokemon-details-label">Bonbon&nbsp;:</span>
+                                <span className="pokemon-details-value">{this.state.pokemon.candy}</span><br/>
+                            </span>
+                        )}
+
+                        {this.state.pokemon.candy_count !== undefined && (
+                            <span>
+                                <span className="pokemon-details-label">Nombre de bonbons&nbsp;:</span>
+                                <span className="pokemon-details-value">{this.state.pokemon.candy_count}</span><br/>
+                            </span>
+                        )}
+
+                        {this.state.pokemon.egg !== "Not in Eggs" && (
+                            <span>
+                                <span className="pokemon-details-label">Œuf&nbsp;:</span>
+                                <span className="pokemon-details-value">{this.state.pokemon.egg}</span><br/>
+                            </span>
+                        )}
+
+                        <span className="pokemon-details-label">Apparitions moyennes&nbsp;:</span>
+                        <span className="pokemon-details-value">{this.state.pokemon.avg_spawns}</span><br/>
+
+                        {this.state.pokemon.spawn_time !== "N/A" && (
+                            <span>
+                                <span className="pokemon-details-label">Temps d'apparition&nbsp;:</span>
+                                <span className="pokemon-details-value">{this.state.pokemon.spawn_time}</span><br/>
+                            </span>
+                        )}
+
+                        {this.state.pokemon.multipliers !== null && (
+                            <span>
+                                <span className="pokemon-details-label">Multiplicateurs&nbsp;:</span>
+                                <span className="pokemon-details-value">
+                                    {this.state.pokemon.multipliers.map(mult => "x" + mult).join(" / ")}
+                                </span><br/>
+                            </span>
+                        )}
+
+                        <span className="pokemon-details-label">Faiblesses&nbsp;:</span>
+                        <span className="pokemon-details-value">
+                            {this.state.pokemon.weaknesses.map(type =>
+                                <span key={type.toLowerCase()} className={`pokemon-type type-${type.toLowerCase()}`}>{type}</span>
+                            )}
+                        </span><br/>
+                    </div>
+                </div>
+
+                <div className="pokemon-details-evolutions">
+                    {this.state.prev_evolution !== undefined && this.state.prev_evolution.map(evolution =>
+                        <PokemonCard key={evolution.id} pokemon={evolution} className="pokemon-details-evolutions-card pokemon-details-evolutions-prev" />
+                    )}
+                    <PokemonCard pokemon={this.state.pokemon} className="pokemon-details-evolutions-card pokemon-details-evolutions-current" />
+                    {this.state.next_evolution !== undefined && this.state.next_evolution.map(evolution =>
+                        <PokemonCard key={evolution.id} pokemon={evolution} className="pokemon-details-evolutions-card pokemon-details-evolutions-next" />
+                    )}
+                </div>
             </div>
         );
     }
