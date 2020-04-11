1a2be5b7df7a34e8cda26175900b0f5a251cf7ac
Aurélien <au2001sfg@gmail.com>
2020-04-11 12:39:21 +0200

    Improve search style

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './SearchBar.css';
+
 export default class SearchBar extends React.Component {
     constructor(props) {
         super(props);
@@ -13,9 +15,22 @@ export default class SearchBar extends React.Component {
         this.autoSubmitTimeout = null;
         this.formRef = React.createRef();
 
+        this._reset = this._reset.bind(this);
         this._submit = this._submit.bind(this);
         this._autoSubmit = this._autoSubmit.bind(this);
         this._changeField = this._changeField.bind(this);
+    }
+
+    _reset(event) {
+        this._autoSubmit();
+
+        this.setState({
+            sort: "",
+            order: "",
+            filter: ""
+        }, () => {
+            this._autoSubmit();
+        });
     }
 
     _submit(event) {
@@ -58,16 +73,30 @@ export default class SearchBar extends React.Component {
 
     render() {
         return (
-            <form ref={this.formRef} onSubmit={this._submit}>
-                <input type="text" name="filter" value={this.state.filter} onChange={this._changeField} />
-                <select name="sort" value={this.state.sort} onChange={this._changeField}>
+            <form ref={this.formRef} onSubmit={this._submit} onReset={this._reset} className="search">
+                <div className="filter-container">
+                    <input type="text" name="filter" placeholder="Rechercher un Pokémon..." value={this.state.filter} onChange={this._changeField} className="filter" />
+                    <label htmlFor="search-reset" className="reset-label">
+                        <img src="/clear.png" alt="Clear" />
+                    </label>
+                    <input type="reset" id="search-reset" hidden />
+                </div>
+                <select name="sort" value={this.state.sort} onChange={this._changeField} className="sort">
                     <option value="">Numéro (par défaut)</option>
                     <option value="name">Nom</option>
                     <option value="rarity">Rareté</option>
                     <option value="height">Hauteur</option>
                     <option value="weight">Poids</option>
                 </select>
-                <input type="checkbox" name="order" checked={this.state.order == null || this.state.order.charAt(0).toLowerCase() !== "d"} onChange={this._changeField} />
+                <label htmlFor="search-order" className="order-label">
+                    {(this.state.order == null || this.state.order.charAt(0).toLowerCase() !== "d") &&
+                        <img src="/order-ascending.png" alt="Ascending order" />
+                    }
+                    {(this.state.order != null && this.state.order.charAt(0).toLowerCase() === "d") &&
+                        <img src="/order-descending.png" alt="Descending order" />
+                    }
+                </label>
+                <input type="checkbox" name="order" id="search-order" checked={this.state.order == null || this.state.order.charAt(0).toLowerCase() !== "d"} onChange={this._changeField} className="order" />
             </form>
         );
     }
